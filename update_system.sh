1a3cfcbf5b4ba0518fce13e143a6914f5808c02d
Victor <victor.gonzalez@kubide.esr>
2022-03-12 15:16:07 +0100

    <> autocommit.sh <>

--- update_system.sh
+++ update_system.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
+#coded by +\N0M4D/+
+OPN_COLOR='\e[1;35m'
+CLOSE_COLOR='\e[0m'
 
-bash titleprinter.sh "M41nt3n4nc3" 2
+function _print_title () {
+    echo -e "$OPN_COLOR"
+    figlet -f letter $1
+    echo -e "$CLOSE_COLOR"
+    sleep ${2:-1}
+}
 
-apt-get update
-apt-get -y upgrade
-apt-get -y autoremove
-apt-get autoclean
+_print_title "M41nt3n4nc3" 2
 
-bash titleprinter.sh "Syst3m Cl34r"
+sudo apt-get update
+sudo apt-get -y upgrade
+sudo apt-get -y autoremove
+sudo apt-get autoclean
+
+_print_title "Syst3m Cl34r"

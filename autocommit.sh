68698426ee2a97cb20b8ecd9c0f388cb87e6a599
N0M4D <>
2022-04-13 19:35:05 +0200

    <> autocommit.sh <>

--- autocommit.sh
+++ autocommit.sh
@@ -14,7 +14,7 @@ currentBranchName=$currentBranch
 
 function _check_dependencies () {
     command -v git >/dev/null 2>&1 || { echo >&2 "GIT not installed. Aborting..."; exit 1; }
-    #command -v figlet >/dev/null 2>&1 || { echo >&2 "FIGLET not installed. Aborting..."; exit 1; }
+    command -v figlet >/dev/null 2>&1 || { echo >&2 "FIGLET not installed. Aborting..."; exit 1; }
 }
 
 function _git_update () {
@@ -30,7 +30,7 @@ function _git_push () {
 
 function _print_title () {
     echo -e "$OPEN_COLOR"
-    figlet -f letter $1
+    figlet $1
     echo -e "$CLOSE_COLOR"
     sleep 0.5
 }

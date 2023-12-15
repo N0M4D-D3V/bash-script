d2be97a857c5e99f39ee744f6a15ebf7ec4450d3
N0M4D <elcadeas28@gmail.com>
2023-12-15 08:17:42 +0100

    display fine tunning

--- open-project.sh
+++ open-project.sh
@@ -17,35 +17,46 @@
 
 base_dir="$HOME/Documents/projects"
 
+echo '<>---> PROJECT OPENER SCRIPT <---<>'
+echo ''
+
 # If no param is provided, list main folders in the base directory
 if [ $# -eq 0 ]; then
-	echo "Listing available directories ..."
-  ls -d "$base_dir"/*/ | while read -r dir; do
-  	echo "$(basename "$dir")"
-  done
+	echo " > No params provided."
+	echo " > Listing available directories at level 0 ..."
+	echo ''
+
+  	ls -d "$base_dir"/*/ | while read -r dir; do
+  		echo "    - $(basename "$dir")"
+  	done
 fi
 
 # If a number is passed as a parameter, list directories up to that level
 if [ $# -eq 1 ] && [ $1 -eq $1 2>/dev/null ]; then
-  max_depth=$1
-  find "$base_dir" -maxdepth $max_depth -type d -exec basename {} \;
+	max_depth=$1
+	echo " > Depth level provided."
+	echo " > Listing available directories at level $max_depth ..."
+	echo ""
+
+  	find "$base_dir" -maxdepth $max_depth -type d -exec sh -c 'echo "    - " "$(basename "$0")"' {} \;
 else
 
   # iterate all the provided params
   for dir_to_open in "$@"
   do
-	  # search the directory
-	  echo "Searching '$dir_to_open' ..."
-	  dir_path=$(find "$base_dir" -type d -name "$dir_to_open" -print -quit)
+	# search the directory
+	echo " > Searching '$dir_to_open' ..."
+	
+	dir_path=$(find "$base_dir" -type d -name "$dir_to_open" -print -quit)
 
-	  # check if directory was found
-	  if [ -n "$dir_path" ];then
+	# check if directory was found
+	if [ -n "$dir_path" ];then
 		  # open the directory
-		  echo "Starting '$dir_path' ..."
+		  echo " > Starting '$dir_path' ..."
 		  code "$dir_path"
-	  else
-		  echo "Directory '$dir_to_open' not found at '$base_dir'."
-	  fi
+	else
+		  echo " <!> Directory '$dir_to_open' not found at '$base_dir' <!>"
+	fi
   done
 fi
 

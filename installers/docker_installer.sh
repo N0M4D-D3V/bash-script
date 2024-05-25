96e5b18daf2a7c6d9d08539bd1ecb40279fd726c
N0M4D <48331382+N0M4D-D3V@users.noreply.github.com>
2024-05-25 18:38:36 +0200

    auto confirm docker install

--- installers/docker_installer.sh
+++ installers/docker_installer.sh
@@ -40,4 +40,4 @@ sudo apt-get update
 echo '<!> Installing docker latest version ...'
 echo ''
 
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
